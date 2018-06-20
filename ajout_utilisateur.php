9c0e82880192c03fd44ad363c79f81a87819ea40
NF17 Etudiant <nf17p102@smestaens24ab.sme.utc>
2018-06-20 19:25:44 +0200

    Rsolution pb ajout util

--- ajout_utilisateur.php
+++ ajout_utilisateur.php
@@ -8,8 +8,8 @@
       <?php
       $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
       $vSql ="INSERT INTO utilisateur(pseudo, nom, prenom, datenaissance, mdp, adresse_numero, adresse_rue, adresse_codepostal,
-          adresse_ville, adresse_pays, type) VALUES ('"$_POST['pseudo']"','"$_POST['nom']"','"$_POST['prenom']"','"$_POST['naissance']"',
-              '"$_POST['mdp']"','"$_POST['num_adresse']"','"$_POST['rue_adresse']"','"$_POST['cp_adresse']"','"$_POST['ville_adresse']"','"$_POST['pays_adresse']"','"$_POST['type']"')";
+          adresse_ville, adresse_pays, type) VALUES ('".$_POST['pseudo']."','".$_POST['nom']."','".$_POST['prenom']."','".$_POST['naissance']."',
+              '".$_POST['mdp']."','".$_POST['num_adresse']."','".$_POST['rue_adresse']."','".$_POST['cp_adresse']."','".$_POST['ville_adresse']."','".$_POST['pays_adresse']."','".$_POST['type']."')";
       $vQuery=pg_query($vConn, $vSql);
       ?>
       <a href="user.php">Retour Utilisateur</a><br>
