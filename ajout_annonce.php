539d3f049cb86f04c1ed7bb55ea87eff1c9c7d67
zk3 <kaddachezakaria@gmail.com>
2018-06-20 18:19:44 +0200

    add

--- ajout_annonce.php
+++ ajout_annonce.php
@@ -9,19 +9,28 @@
       <form method="post" action="ajouter_annonce.php">
         <p>
           <label for="titre_annonce"> Titre de l'annonce : </label>
-          <input type="text" name="titre" id="titre_annonce" maxlength="30" required/>
+          <input type="text" name="titre_annonce" id="titre_annonce" maxlength="30" required/>
         </p>
         <p>
-          <label for="titre_annonce"> Description de l'annonce : </label>
-          <input type="text" name="description" id="description_annonce" />
+          <label for="description_annonce"> Description de l'annonce : </label>
+          <input type="text" name="description_annonce" id="description_annonce" />
         </p>
         <p>
           <label for="categorie_annonce"> Categorie de l'annonce : </label>
           <select name="categorie_annonce" id="categorie_annonce"  required>
-            <optgroup label="categorie_annonce">
+            <optgroup label="Categorie de l'annonce">
               <option value="informatique"> Informatique </option>
+              <option value="agriculture"> Agriculture </option>
             </optgroup>
           </select>
+        </p>
+        <p>
+          <label for="enseigne_annonce"> De quelle enseigne : </label>
+          <input type="text" name="enseigne_annonce" id="enseigne_annonce" />
+        </p>
+        <p>
+          <label for="photo_annonce"> Ajouter une photo  : </label>
+          <input type="file" name="photo_annonce" id="photo_annonce" />
         </p>
       </form>
     </body>
