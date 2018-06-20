7d9518e137e4dcb8508c221d15588c7ec8c7c6cd
NF17 Etudiant <nf17p088@smestaens24ab.sme.utc>
2018-06-20 18:01:13 +0200

    commentaire

--- commentaire.php
+++ commentaire.php
@@ -2,9 +2,41 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Page d'accueil</title>
+        <title>Page Commentaire</title>
     </head>
     <body>
-        Coucou <3
+        <h1> Commentaires </h1>
+          <h2> Ajout </h2>
+
+              <? $connexion = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
+
+              $annonce = "SELECT * FROM annonces";
+              $resultsetannonce = $connexion->prepare($annonce);
+              $resultsetannonce->execute();
+
+              while ($row = $resultsetannonce->fetch(PDO::FETCH_ASSOC)) {
+	               echo $row['nom'];
+	               echo " ";
+               }
+
+               $user = "SELECT * FROM utilisateur";
+               $resultsetuser = $connexion->prepare($user);
+               $resultsetuser->execute();
+
+               while ($row = $resultsetuser->fetch(PDO::FETCH_ASSOC)) {
+ 	               echo $row['nom'];
+ 	               echo " ";
+                }
+               ?>
+
+            <FORM METHOD='POST' ACTION='ajout_commentaire.php'>
+              Annonce : <br>
+              <input type="number" name="annonce"> <br>
+              Utilisateur : <br>
+              <input type="text" name="user"> <br>
+              Commentaire : <br>
+              <input type="text" name="commentaire"> <br>
+            </FORM>
+
     </body>
 </html>
