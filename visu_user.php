959b16cf6cf6fe94d284a92e11706f01096849ab
Hugo Le Moine <hugo.le-moine@outlook.fr>
2018-06-20 18:07:47 +0200

    table pour chaque utilisateur

--- visu_user.php
+++ visu_user.php
@@ -5,7 +5,6 @@
         <title>Liste utilisateurs</title>
     </head>
     <body>
-        <table>
             <?php
 
             $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
@@ -14,15 +13,16 @@
             $vQuery=pg_query($vConn, $vSql);
             while ($vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC)) {
                 echo "
+                <table>
                 <tr><td>Nom</td><td>".$vResult[nom]."</td></tr>
                 <tr><td>Prenom</td><td>".$vResult[prenom]."</td></tr>
                 <tr><td>Date de naissance</td><td>".$vResult[datenaissance]."</td></tr>
                 <tr><td>Adresse</td><td>".$vResult[adresse_numero]." ".$vResult[adresse_rue]." ".$vResult[adresse_codepostal]." ".$vResult[adresse_ville]." ".$vResult[adresse_pays]."</td></tr>
                 <tr><td>Pays</td><td>".$vResult[adresse_pays]."</td></tr>
-                <tr><td>Type</td><td>".$vResult[type]."</td></tr>";
+                <tr><td>Type</td><td>".$vResult[type]."</td></tr>
+                </table>";
             }
             pg_close($conn)
             ?>
-        </table>
     </body>
 </html>
