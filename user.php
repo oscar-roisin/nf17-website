2bd7b1e2d3b3b9ed5b6b5d0c41832d4b846093cf
NF17 Etudiant <nf17p102@smestaens24ab.sme.utc>
2018-06-20 17:58:27 +0200

    "Form ajout util"

--- user.php
+++ user.php
@@ -2,9 +2,36 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Page d'accueil</title>
+        <title>Page User</title>
     </head>
     <body>
-        Coucou <3
+      <h1>Utilisateurs</h1>
+      <h2>Ajout</h2>
+        <form METHOD='POST' ACTION='ajout_utilisateur.php' >
+          Pseudo:<br>
+          <input type="text" name="pseudo"><br>
+          NOM:<br>
+          <input type="text" name="nom"><br>
+          Prenom:<br>
+          <input type="text" name="prenom"><br>
+          Date de naissance:<br>
+          <input type="date" name="naissance"><br>
+          Password:<br>
+          <input type="password" name="mdp"><br>
+          Adresse:<br>
+          N°<input type="number" name="num_adresse">
+          Rue<input type="text" name"rue_adresse"><br>
+          CP<input type="number" name="cp_adresse">
+          Ville<input type="text" name="ville_adresse"><br>
+          Pays<input <type="text" name="pays_adresse">
+          Type:<br>
+          <input type="radio" name="type" value="utilisateur" checked> Utilisateur<br>
+          <input type="radio" name="type" value="administrateur"> Administrateur<br>
+          <input type="submit" name="ajout_utilisateur">
+          <input type="reset">
+        </form>
+      <h2>Visualisation</h2>
+        <FORM METHOD='POST' ACTION='visu_user.php' >
+        </form>
     </body>
 </html>
