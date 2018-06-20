e77ba60fed1a48ae523d6dbc53825a772be79d85
NF17 Etudiant <nf17p088@smestaens24ab.sme.utc>
2018-06-20 17:06:09 +0200

    base

--- user.php
+++ user.php
@@ -1,1 +1,10 @@
-<html></html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Page d'accueil</title>
+    </head>
+    <body>
+        Coucou <3
+    </body>
+</html>
