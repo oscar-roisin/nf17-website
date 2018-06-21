598ab752b51998cf8860e578a9831ff277ebe73a
Hugo Le Moine <hugo.le-moine@outlook.fr>
2018-06-21 12:07:08 +0200

    Ajout enseigne + visu

---
+++ enseigne.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Page Enseigne</title>
+    </head>
+    <body>
+      <h1>Enseignes</h1>
+      <h2>Ajout</h2>
+        <form METHOD='POST' ACTION='ajout_enseigne.php' >
+          Nom de l'enseigne:<br>
+          <input type="text" name="nomenseigne"><br>
+          Site web:<br>
+          <input type="text" name="website"><br>
+          Email:<br>
+          <input type="text" name="email"><br>
+          Telephone:<br>
+          <input type="text" name="telephone"><br>
+          Rue:<br>
+          <input type="text" name="localisationrue"><br>
+          Code Postal<br>
+          <input type="text" name="localisationcodepostal">
+          Ville<br>
+          <input type="text" name="localisationville">
+          Pays<br>
+          <input type="text" name"localisationpays"><br>
+          <input type="submit" name="ajout_enseigne">
+          <input type="reset">
+        </form>
+      <h2>Visualisation</h2>
+        <FORM METHOD='POST' ACTION='visu_enseigne.php' >
+          <?php
+          $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
+          $vSql ="SELECT * FROM enseigne";
+          $vQuery=pg_query($vConn, $vSql);
+          echo"<select name='enseigne'>";
+          while ($vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC)) {
+            echo "<option value=".$vResult['nomenseigne'].">".$vResult['nomenseigne']."</option>";
+            }
+            echo"</select>";
+          pg_close($vConn);
+          ?>
+          <input type="submit" name="visu_enseigne">
+        </form>
+        <a href="index.html">MENU</a>
+    </body>
+</html>
