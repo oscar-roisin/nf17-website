95ea68d69d1b6db8d462d5cd91922d600101b998
dictusla <38868026+dictusla@users.noreply.github.com>
2018-06-22 11:42:08 +0200

    cor pseudo signalemetn

---
+++ signalement.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Ajouter un like</title>
+    </head>
+    <body>
+
+      <a href="index.html">MENU</a>
+      <a href="annonce.php">Retour aux annonces</a>
+      <?php
+        $pseudo = $_POST['user'];
+        $raison = $_POST['raison_signalement'];
+        $idcommentaire = $_POST['idcommentaire'];
+
+        $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
+        $vSql = "INSERT INTO Signaler (pseudo, idCommentaire, raison) VALUES ($pseudo $idcommentaire, $raison);
+;";
+        $vQuery = pg_query($vConn, $vSql);
+        pg_close($vConn);
+
+      ?>
+    </body>
+</html>
