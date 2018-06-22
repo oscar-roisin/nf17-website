1abf95c4138b0aa4b48cfe4103614107f84aac49
Hugo Le Moine <hugo.le-moine@outlook.fr>
2018-06-22 13:46:17 +0200

    maj

--- stats.php
+++ stats.php
@@ -5,7 +5,7 @@
         <title>Statistiques</title>
     </head>
     <body>
-		<a href="index.html">MENU</a><br>
+		<a href="index.html">MENU</a>
         <table>
             <?php
 
@@ -26,20 +26,30 @@
             $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
             echo "<tr><td>Nombre de commentaires</td><td>".$vResult[0]."</td></tr>";
 
-            echo "<br>";
-
             $vSql ="SELECT COUNT(*) FROM Liker";
             $vQuery=pg_query($vConn, $vSql);
             $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
             echo "<tr><td>Nombre de likes</td><td>".$vResult[0]."</td></tr>";
-            echo "<br>";
+
+            echo "</table><br>";
+
+            echo "<h4>Liste des utilisateurs triés par nombre d'annonces postées</h4>"
+
             $vSql ="SELECT pseudo, COUNT(idannonce) as nbAnnonces FROM Annonce a JOIN Utilisateur u ON a.pseudoUtilisateur=u.pseudo GROUP BY pseudo ORDER BY nbAnnonces";
             $vQuery=pg_query($vConn, $vSql);
             echo "<table><tr><td>Pseudo</td><td>Annonces postées</td></tr>";
             while ($vResult = pg_fetch_array($vQuery, null, PGSQL_NUM)) {
               echo "<tr><td>".$vResult[0]."</td><td>".$vResult[1]."</td></tr>";
               }
-            echo "</table>";
+
+            echo "<br><h4>Liste des utilisateurs triés par nombre de commentaires postés</h4>"
+
+            $vSql ="SELECT pseudo, COUNT(idCommentaire) as nbCommentaires FROM Commentaire c JOIN Utilisateur u ON c.pseudo=u.pseudo GROUP BY pseudo ORDER BY nbCommentaires";
+            $vQuery=pg_query($vConn, $vSql);
+            echo "<table><tr><td>Pseudo</td><td>Comemntaires postées</td></tr>";
+            while ($vResult = pg_fetch_array($vQuery, null, PGSQL_NUM)) {
+              echo "<tr><td>".$vResult[0]."</td><td>".$vResult[1]."</td></tr>";
+              }
             pg_close($vConn);
             ?>
         </table>
