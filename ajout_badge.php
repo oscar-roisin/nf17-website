c86314953eea619a2d8b75e93b89f88dafc1776d
dictusla <38868026+dictusla@users.noreply.github.com>
2018-06-22 15:10:38 +0200

    cor badge

--- ajout_badge.php
+++ ajout_badge.php
@@ -7,17 +7,18 @@
     <body>
 
       <a href="http://tuxa.sme.utc/~nf17p095/">MENU</a>
+      <a href="badge.php">Retour badge</a>
       <?php
 
         $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
 
         $titre = $_POST['titre_badge'];
         $description = $_POST['description_badge'];
-        $logo = $_POST['logo_badge'];
         $duree = $_POST['duree_badge'];
-        $definitif = 0;
-        $vSql = "INSERT INTO badge(titreBadge, descriptionBadge, logo, duree, definitif )
-                  values('$titre', '$description', '$logo', '$duree', '$definitif') ;"    ;
+
+        $definitif = !$duree;
+        $vSql = "INSERT INTO badge(titreBadge, descriptionBadge, duree, definitif )
+                  values('".$titre."', '".$description."', '".$duree."', '".$definitif."') ;"    ;
 
 
         $vQuery = pg_query($vConn, $vSql);
