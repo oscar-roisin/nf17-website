a5448fd25c974bd9bdc054973f57140f0347fb71
zk3 <kaddachezakaria@gmail.com>
2018-06-22 11:42:55 +0200

    Ajouter Annonce Termine

--- annonce.php
+++ annonce.php
@@ -13,14 +13,14 @@
           <label for="type_annonce"> Type de l'annonce : </label>
           <select name="type_annonce" id="type_annonce"  required>
             <optgroup label="Type de l'annonce">
-              <option value="CodePromo"> Code de Promotion </option>
+              <option value="CodePromo" default> Code de Promotion </option>
               <option value="BonneAffaire"> Bonne Affaire </option>
             </optgroup>
           </select>
         </p>
         <p>
           <label for="titre_annonce"> Titre de l'annonce : </label>
-          <input type="text" name="titre_annonce" id="titre_annonce" maxlength="30" required/>
+          <input type="text" name="titre_annonce" id="titre_annonce" maxlength="30" required  />
         </p>
         <p>
           <label for="description_annonce"> Description de l'annonce : </label>
@@ -55,10 +55,58 @@
           <label for="date_expiration_annonce"> Date Expiration : </label>
           <input type="date" name="date_expiration_annonce" id="date_expiration_annonce" />
         </p>
+        <p>
+          <label for="code_annonce" class="code_annonce" > Code Promo : </label>
+          <input type="number" name="code_annonce" id="code_annonce" class="code_annonce" />
+        </p>
+        <p>
+          <label class = "affaire_annonce" for="new_price_annonce" hidden=TRUE > Nouveau Prix : </label>
+          <input class = "affaire_annonce" type="number" name="new_price_annonce" id="new_price_annonce" hidden=TRUE />
+        </p>
+        <p>
+          <label class = "affaire_annonce" for="old_price_annonce" hidden=TRUE > Ancien Prix  : </label>
+          <input class = "affaire_annonce" type="number" name="old_price_annonce" id="old_price_annonce" hidden=TRUE />
+        </p>
+        <p>
+          <label class = "affaire_annonce" for="port_annonce" hidden=TRUE > Frais de Port : </label>
+          <input class = "affaire_annonce" type="number" name="port_annonce" id="port_annonce" hidden=TRUE />
+        </p>
 
         <input type="submit" value="Envoyer" />
       </form>
+      <script>
+        var type = document.getElementById('type_annonce');
+          type.value = "CodePromo";
+        var code = document.getElementsByClassName('code_annonce');
+        var affaire = document.getElementsByClassName('affaire_annonce');
+        var i ;
+        type.addEventListener('change', function() {
+          if(type.value == "CodePromo"){
+            for(i = 0; i < code.length; i++){
+              code[i].removeAttribute("hidden");
+              code[i].value = 0;
+            }
 
+            for(i = 0; i < affaire.length; i++){
+              affaire[i].setAttribute("hidden","true");
+              affaire[i].value = -1;
+            }
+
+          }
+          else{
+            for(i = 0; i < affaire.length; i++){
+              affaire[i].removeAttribute("hidden");
+              affaire[i].value = 0;
+            }
+            for(i = 0; i < code.length; i++){
+              code[i].setAttribute("hidden","true");
+              code[i].value = -1;
+            }
+          }
+        });
+
+
+      </script>
       <h1>Visualisation</h1>
         <FORM METHOD='POST' ACTION='visu_annonce.php' >
           <?php
