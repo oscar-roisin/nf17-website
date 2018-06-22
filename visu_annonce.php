c71cb25e5e0820df2897136a8ccf3a05958c015e
dictusla <38868026+dictusla@users.noreply.github.com>
2018-06-22 09:28:18 +0200

    cor nom attribust visu annonce

--- visu_annonce.php
+++ visu_annonce.php
@@ -15,19 +15,19 @@
             while ($vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC)) {
                 echo "
                 <table>
-                <tr><td>Titre</td><td>".$vResult['titreAnnonce']."</td></tr>
-                <tr><td>Description</td><td>".$vResult['descriptionAnnonce']."</td></tr>
+                <tr><td>Titre</td><td>".$vResult['titreannonce']."</td></tr>
+                <tr><td>Description</td><td>".$vResult['descriptionannonce']."</td></tr>
                 <tr><td>Categorie</td><td>".$vResult['categorie']."</td></tr>
-                <tr><td>Enseigne</td><td>".$vResult['nomEnseigne']."</td></tr>
+                <tr><td>Enseigne</td><td>".$vResult['nomenseigne']."</td></tr>
                 <tr><td>Type</td><td>".$vResult['type']."</td></tr>
                 <tr><td>Code</td><td>".$vResult['code']."</td></tr>
-                <tr><td>Nouveau Prix</td><td>".$vResult['newPrice']."</td></tr>
-                <tr><td>Prix initial</td><td>".$vResult['initPrice']."</td></tr>
-                <tr><td>Frais de port</td><td>".$vResult['fraisPort']."</td></tr>
+                <tr><td>Nouveau Prix</td><td>".$vResult['newprice']."</td></tr>
+                <tr><td>Prix initial</td><td>".$vResult['initprice']."</td></tr>
+                <tr><td>Frais de port</td><td>".$vResult['fraisport']."</td></tr>
                 <tr><td>Lien</td><td><a href=".$vResult['lien'].">".$vResult['lien']."</a></td></tr>
-                <tr><td>Date de soumission</td><td>".$vResult['dateSubmit']."</td></tr>
-                <tr><td>Date de début</td><td>".$vResult['dateDebut']."</td></tr>
-                <tr><td>Date d'expiration</td><td>".$vResult['dateExpiration']."</td></tr>
+                <tr><td>Date de soumission</td><td>".$vResult['datesubmit']."</td></tr>
+                <tr><td>Date de début</td><td>".$vResult['datedebut']."</td></tr>
+                <tr><td>Date d'expiration</td><td>".$vResult['dateexpiration']."</td></tr>
 
                 <tr><td>Etat</td><td>";
                 if($vResult[etat]) echo "Validée";
