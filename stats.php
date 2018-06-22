743c96bd4180f5dc9585e1403d5119b867f0608b
Hugo Le Moine <hugo.le-moine@outlook.fr>
2018-06-22 14:27:05 +0200

    maj

--- stats.php
+++ stats.php
@@ -35,7 +35,7 @@
 
             echo "<h4>Liste des utilisateurs triés par nombre d'annonces postées</h4>";
 
-            $vSql ="SELECT u.pseudo, COUNT(idannonce) as nbAnnonces FROM Annonce a JOIN Utilisateur u ON a.pseudoUtilisateur=u.pseudo GROUP BY pseudo ORDER BY nbAnnonces";
+            $vSql ="SELECT pseudo, COUNT(idannonce) as nbAnnonces FROM Annonce a JOIN Utilisateur u ON a.pseudoUtilisateur=u.pseudo GROUP BY pseudo ORDER BY nbAnnonces";
             $vQuery=pg_query($vConn, $vSql);
             echo "<table><tr><td>Pseudo</td><td>Annonces postées</td></tr>";
             while ($vResult = pg_fetch_array($vQuery, null, PGSQL_NUM)) {
@@ -44,7 +44,7 @@
 
             echo "<br><h4>Liste des utilisateurs triés par nombre de commentaires postés</h4>";
 
-            $vSql ="SELECT pseudo, COUNT(idCommentaire) as nbCommentaires FROM Commentaire c JOIN Utilisateur u ON c.pseudo=u.pseudo GROUP BY u.pseudo ORDER BY nbCommentaires";
+            $vSql ="SELECT u.pseudo, COUNT(idCommentaire) as nbCommentaires FROM Commentaire c JOIN Utilisateur u ON c.pseudo=u.pseudo GROUP BY u.pseudo ORDER BY nbCommentaires";
             $vQuery=pg_query($vConn, $vSql);
             echo "<table><tr><td>Pseudo</td><td>Commentaires postées</td></tr>";
             while ($vResult = pg_fetch_array($vQuery, null, PGSQL_NUM)) {
