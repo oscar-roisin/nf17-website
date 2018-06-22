16bdf71674faf2211bfe30def89cbf07d30f8baa
Hugo Le Moine <hugo.le-moine@outlook.fr>
2018-06-22 13:42:39 +0200

    modif SQL

--- code_promo.php
+++ code_promo.php
@@ -11,7 +11,7 @@
             <tr><td>ID</td><td>Titre</td><td>Code promo</td></tr>
           <?php
             $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
-            $vSql = "SELECT pseudo, nom, prenom FROM vAnnonceCP";
+            $vSql = "SELECT idAnnonce, titreAnnonce, code FROM vAnnonceCP";
             $vQuery = pg_query($vConn, $vSql);
             while ($vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC)) {
                 echo "<tr><td>".$vResult['idAnnonce']."</td><td>".$vResult['titreAnnonce']."</td><td>".$vResult['code']."</tr>";
