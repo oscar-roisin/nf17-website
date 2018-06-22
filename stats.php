48c7453aefc310e20b3925f57d5dd254f52c89eb
Hugo Le Moine <hugo.le-moine@outlook.fr>
2018-06-22 12:11:47 +0200

    maj

--- stats.php
+++ stats.php
@@ -11,31 +11,31 @@
 
             $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
 
-            $vSql ="SELECT COUNT(*) AS nbAnnonces FROM Annonce";
+            $vSql ="SELECT COUNT(*) FROM Annonce";
             $vQuery=pg_query($vConn, $vSql);
             $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
             echo "<tr><td>Nombre d'annonces</td><td>".$vResult[0]."</td></tr>";
 
-            $vSql ="SELECT COUNT(*) AS nbUtilisateurs FROM Utilisateur";
+            $vSql ="SELECT COUNT(*) FROM Utilisateur";
             $vQuery=pg_query($vConn, $vSql);
             $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
             echo "<tr><td>Nombre d'utilisateurs</td><td>".$vResult[0]."</td></tr>";
 
-            $vSql ="SELECT COUNT(*) AS nbCommentaires FROM Commentaire";
+            $vSql ="SELECT COUNT(*) FROM Commentaire";
             $vQuery=pg_query($vConn, $vSql);
             $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
             echo "<tr><td>Nombre de commentaires</td><td>".$vResult[0]."</td></tr>";
 
-            $vSql ="SELECT COUNT(*) AS nbLikes FROM Liker";
+            $vSql ="SELECT COUNT(*) FROM Liker";
             $vQuery=pg_query($vConn, $vSql);
             $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
             echo "<tr><td>Nombre de likes</td><td>".$vResult[0]."</td></tr>";
 
-            $vSql ="SELECT pseudo, COUNT(idannonce) AS nbAnnonces FROM Annonce a JOIN Utilisateur u ON a.pseudoUtilisateur=u.pseudo GROUP BY pseudo";
+            $vSql ="SELECT pseudo, COUNT(idannonce) FROM Annonce a JOIN Utilisateur u ON a.pseudoUtilisateur=u.pseudo GROUP BY pseudo";
             $vQuery=pg_query($vConn, $vSql);
             echo "<table><tr><td>Pseudo</td><td>Annonces postées</td></tr>";
-            while ($vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC)) {
-              echo "<tr><td>".$vResult['pseudo']."</td><td>".$vResult['nbAnnonces']."</td></tr>";
+            while ($vResult = pg_fetch_array($vQuery, null, PGSQL_NUM)) {
+              echo "<tr><td>".$vResult[0]."</td><td>".$vResult[1]."</td></tr>";
               }
             echo "</table>";
             pg_close($vConn);
