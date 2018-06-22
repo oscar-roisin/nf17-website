8f89933d038134d81406ba26383f005fc9885a3e
Hugo Le Moine <hugo.le-moine@outlook.fr>
2018-06-22 12:01:30 +0200

    maj stats

--- stats.php
+++ stats.php
@@ -18,18 +18,18 @@
 
             $vSql ="SELECT COUNT(*) AS nbUtilisateurs FROM Utilisateur";
             $vQuery=pg_query($vConn, $vSql);
-            $vResult = pg_fetch_array($vQuery, null, PGSQL_BOTH);
+            $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
             echo "<tr><td>Nombre d'utilisateurs</td><td>".$vResult[0]."</td></tr>";
 
             $vSql ="SELECT COUNT(*) AS nbCommentaires FROM Commentaire";
             $vQuery=pg_query($vConn, $vSql);
-            $vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC);
-            echo "<tr><td>Nombre de commentaires</td><td>".$vResult[nbCommentaires]."</td></tr>";
+            $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
+            echo "<tr><td>Nombre de commentaires</td><td>".$vResult[0]."</td></tr>";
 
             $vSql ="SELECT COUNT(*) AS nbLikes FROM Liker";
             $vQuery=pg_query($vConn, $vSql);
-            $vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC);
-            echo "<tr><td>Nombre de likes</td><td>".$vResult['nbLikes']."</td></tr>";
+            $vResult = pg_fetch_array($vQuery, null, PGSQL_NUM);
+            echo "<tr><td>Nombre de likes</td><td>".$vResult[0]."</td></tr>";
 
             pg_close($vConn);
             ?>
