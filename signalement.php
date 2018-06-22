6490d648692e02963d2b98e07942d7a6ec46707e
dictusla <38868026+dictusla@users.noreply.github.com>
2018-06-22 13:19:57 +0200

    cor like

--- signalement.php
+++ signalement.php
@@ -6,21 +6,21 @@
     </head>
     <body>
 
+      <a href="index.html">MENU</a>
+      <a href="annonce.php">Retour aux annonces</a><br>
+      
       <?php
         $pseudo = $_POST['user'];
         $raison = $_POST['raison_signalement'];
         $idcommentaire = $_POST['idcommentaire'];
 
         $vConn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p095 user=nf17p095 password=sMdOMm7S");
-        $vSql = "INSERT INTO Signaler (pseudo, idCommentaire, raison) VALUES ('".$pseudo."', '".$idcommentaire."','".$raison."');
-;";
+        $vSql = "INSERT INTO Signaler (pseudo, idCommentaire, raison) VALUES ('".$pseudo."', '".$idcommentaire."','".$raison."');";
         $vQuery = pg_query($vConn, $vSql);
         pg_close($vConn);
 
       ?>
-      
+
       <p>Commentaire signalé.</p>
-      <a href="index.html">MENU</a>
-      <a href="annonce.php">Retour aux annonces</a><br>
     </body>
 </html>
